52429e830741ee4111b00d6088edbb80374fb068
obr-note <k33mdukb9n@yahoo.co.jp>
2021-08-01 00:15:17 +0900

    6-6

--- web.go
+++ web.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+  "log"
   "net/http"
+  "text/template"
 )
 
 func main() {
-  http.ListenAndServe("", http.FileServer(http.Dir(".")))
+  html := `<html>
+  <body>
+  <h1>HELLO</h1>
+  <p>This is sample message.</p>
+  </body></html>`
+  tf, er := template.New("index").Parse(html)
+  if er != nil {
+    log.Fatal(er)
+  }
+  hh := func(w http.ResponseWriter, rq *http.Request) {
+    er = tf.Execute(w, nil)
+    if er != nil {
+      log.Fatal(er)
+    }
+  }
+
+  http.HandleFunc("/hello", hh)
+
+  http.ListenAndServe("", nil)
 }
