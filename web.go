eaccb4dd3dbaeff7fad8a38367fcdd8e9105e845
obr-note <k33mdukb9n@yahoo.co.jp>
2021-08-01 15:05:42 +0900

    6-9

--- web.go
+++ web.go
@@ -6,19 +6,62 @@ import (
   "text/template"
 )
 
-func main() {
-  tf, er := template.ParseFiles("templates/hello.html")
-  if er != nil {
-    tf, _ = template.New("index").Parse("<html><body><h1>NO TEMPLATE.</h1></body></html>")
-  }
-  hh := func(w http.ResponseWriter, rq *http.Request) {
-    er = tf.Execute(w, nil)
-    if er != nil {
-      log.Fatal(er)
-    }
-  }
+type Temps struct {
+  notemp *template.Template
+  indx *template.Template
+  helo *template.Template
+}
 
-  http.HandleFunc("/hello", hh)
+func notemp() *template.Template {
+  src := "<html><body><h1>NO TEMPLATE.</h1></body></html>"
+  tmp, _ := template.New("index").Parse(src)
+  return tmp
+}
+
+func setupTemp() *Temps {
+  temps := new(Temps)
+
+  temps.notemp = notemp()
+
+  indx, er := template.ParseFiles("templates/index.html")
+  if er != nil {
+    indx = temps.notemp
+  }
+  temps.indx = indx
+
+  helo, er := template.ParseFiles("templates/hello.html")
+  if er != nil {
+    helo = temps.notemp
+  }
+  temps.helo = helo
+
+  return temps
+}
+
+func index(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
+  er := tmp.Execute(w, nil)
+  if er != nil {
+    log.Fatal(er)
+  }
+}
+
+func hello(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
+  er := tmp.Execute(w, nil)
+  if er != nil {
+    log.Fatal(er)
+  }
+}
+
+func main() {
+  temps := setupTemp()
+
+  http.HandleFunc("/", func(w http.ResponseWriter, rq *http.Request) {
+    index(w, rq, temps.indx)
+  })
+
+  http.HandleFunc("/hello", func(w http.ResponseWriter, rq *http.Request) {
+    hello(w, rq, temps.helo)
+  })
 
   http.ListenAndServe("", nil)
 }
